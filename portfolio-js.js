a154fefb0e5ccebca9ebec55ee3aac0af77ea96e
xpack94 <xpack94@gmail.com>
2016-12-02 12:22:41 -0500

    Update portfolio-js.js

--- portfolio-js.js
+++ portfolio-js.js
@@ -1,1 +1,31 @@
 
+	$(function(){
+
+		var scroll_pos = 0;
+		var scroll_time;
+
+		$(window).scroll(function() {
+    		clearTimeout(scroll_time);
+    		var current_scroll = $(window).scrollTop();
+
+    		if (current_scroll >= $('#header').outerHeight()) {
+        		if (current_scroll <= scroll_pos) {
+            		$('.hidden-menu').removeClass('hidden');    
+        		}
+        			else {
+            			$('.hidden-menu').addClass('hidden');  
+        			}
+        			
+   				 }else{
+    				$(".hidden-menu").addClass("hidden");
+   	 				}
+
+   
+
+
+    scroll_time = setTimeout(function() {
+        scroll_pos = $(window).scrollTop();
+    }, 100);
+});
+
+	});
